65f7dea9aed116d9647cffb1e8e8e1f4d79035ce
gangadhar <puramgangadhar006@gmail.com>
2024-04-06 22:40:08 +0530

    initial commit

---
+++ src/test/java/org/blogger/bloggerapp/BloggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blogger.bloggerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
